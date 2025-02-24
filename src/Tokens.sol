a644c3d3f4b71d060b4795c540d184b4515086cf
Danzrrr <graita.sukma@gmail.com>
2025-02-24 20:50:29 +0700

    feat: testing token contract

---
+++ src/Tokens.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >= 0.8.25 <0.9.0;
+
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+import { EnumerableMap } from "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
+
+contract Tokens is AccessControl {
+    using EnumerableMap for EnumerableMap.UintToAddressMap;
+
+    struct AllowedToken {
+        uint8 decimals;
+        string symbol;
+        string name;
+    }
+
+    uint256 private constant CHAIN_ID = 84_532;
+    mapping(address tokenAddress => AllowedToken token) public allowedTokens;
+    bytes32 private constant EDITOR_ROLE = keccak256("EDITOR_ROLE");
+    EnumerableMap.UintToAddressMap private tokens;
+
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(EDITOR_ROLE, msg.sender);
+    }
+
+    error TokenValidationError(string message);
+
+    event TokenAdded(address indexed tokenAddress, uint8 decimals, uint256 chain, string symbol, string name);
+    event TokenRemoved(address indexed tokenAddress);
+
+    /**
+     * @dev Add a new token to the allowed tokens list.
+     * Reverts with a `TokenValidationError` if the token address is zero,
+     * the token decimals are zero, or the token symbol or name are empty.
+     * Also reverts if the token already exists in the list.
+     * @param tokenAddress of the token
+     * @param decimals of the token
+     * @param symbol of the token
+     * @param name of the token
+     */
+    function addAllowedToken(
+        address tokenAddress,
+        uint8 decimals,
+        string memory symbol,
+        string memory name
+    )
+        external
+        onlyRole(EDITOR_ROLE)
+    {
+        if (tokenAddress == address(0)) {
+            revert TokenValidationError("Token address cannot be zero");
+        }
+        if (decimals == 0) {
+            revert TokenValidationError("Token decimals cannot be zero");
+        }
+        if (bytes(symbol).length == 0 || bytes(name).length == 0) {
+            revert TokenValidationError("Token symbol or name cannot be empty");
+        }
+        if (_isTokenAvailable(tokenAddress)) {
+            revert TokenValidationError("Token already exists");
+        }
+
+        allowedTokens[tokenAddress] = AllowedToken(decimals, symbol, name);
+        tokens.set(tokens.length(), tokenAddress);
+
+        emit TokenAdded(tokenAddress, decimals, CHAIN_ID, symbol, name);
+    }
+
+    /**
+     * @dev Removes a token from the allowed tokens list.
+     * Reverts with a `TokenValidationError` if the token does not exist in the list.
+     * @param tokenAddress The address of the token contract to remove
+     */
+    function removeAllowedToken(address tokenAddress) external onlyRole(EDITOR_ROLE) {
+        if (tokenAddress == address(0)) {
+            revert TokenValidationError("Token address cannot be zero");
+        }
+        uint256 index = _getTokenIndex(tokenAddress);
+        delete allowedTokens[tokenAddress];
+        tokens.remove(index);
+
+        emit TokenRemoved(tokenAddress);
+    }
+
+    /**
+     * @notice Retrieves the details of an allowed token.
+     * @param tokenAddress The address of the token to retrieve.
+     * @return AllowedToken The details of the allowed token.
+     */
+    function getAllowedToken(address tokenAddress) external view returns (AllowedToken memory) {
+        return allowedTokens[tokenAddress];
+    }
+
+    /**
+     * @dev Checks if a token is available in the allowed tokens list.
+     * @param tokenAddress The address of the token to check.
+     * @return bool Returns true if the token is available, false otherwise.
+     */
+    function _isTokenAvailable(address tokenAddress) internal view returns (bool) {
+        return allowedTokens[tokenAddress].decimals != 0;
+    }
+
+    /**
+     * @dev Internal function to get the index of a token in the `tokens` mapping by its address.
+     * Iterates through the `tokens` mapping to find the token address and returns its index.
+     * Reverts with a `TokenValidationError` if the token does not exist in the mapping.
+     *
+     * @param tokenAddress The address of the token to find.
+     * @return index The index of the token in the `tokens` mapping.
+     */
+    function _getTokenIndex(address tokenAddress) internal view returns (uint256 index) {
+        for (uint256 i = 0; i < tokens.length(); i++) {
+            (uint256 key, address value) = tokens.at(i);
+            if (value == tokenAddress) {
+                return key;
+            }
+        }
+        revert TokenValidationError("Token does not exist");
+    }
+}
---
+++ test/Tokens.t.sol
@@ -0,0 +1,119 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >= 0.8.25 <0.9.0;
+
+import { Test } from "forge-std/src/Test.sol";
+import { Tokens } from "../src/Tokens.sol";
+
+contract TokensTest is Test {
+    Tokens private tokens;
+    uint256 private constant CHAIN_ID = 84_532;
+
+    function setUp() public {
+        tokens = new Tokens();
+    }
+
+    function testFailAddAllowedTokenZeroAddress() public {
+        vm.expectRevert("Token address cannot be zero");
+        tokens.addAllowedToken(address(0), 6, "USDT", "Tether USD");
+    }
+
+    function testFailAddAllowedTokenZeroDecimals() public {
+        vm.expectRevert("Token decimals cannot be zero");
+        tokens.addAllowedToken(address(1), 0, "USDT", "Tether USD");
+    }
+
+    function testFailAddAllowedTokenEmptySymbol() public {
+        vm.expectRevert("Token symbol or name cannot be empty");
+        tokens.addAllowedToken(address(1), 6, "", "Tether USD");
+    }
+
+    function testFailAddAllowedTokenEmptyName() public {
+        vm.expectRevert("Token symbol or name cannot be empty");
+        tokens.addAllowedToken(address(1), 6, "USDT", "");
+    }
+
+    function testFailAddAllowedTokenAlreadyExists() public {
+        tokens.addAllowedToken(address(1), 6, "USDT", "Tether USD");
+        vm.expectRevert("Token already exists");
+        tokens.addAllowedToken(address(1), 6, "USDT", "Tether USD");
+    }
+
+    function testAddAllowedToken() public {
+        vm.expectEmit();
+        uint8 decimals = 6;
+        string memory symbol = "USDT";
+        string memory name = "Tether USD";
+        emit Tokens.TokenAdded(address(1), decimals, CHAIN_ID, symbol, name);
+        tokens.addAllowedToken(address(1), decimals, symbol, name);
+    }
+
+    function testFailRemoveAllowedTokenZeroAddress() public {
+        vm.expectRevert("Token address cannot be zero");
+        tokens.removeAllowedToken(address(0));
+    }
+
+    function testFailRemoveAllowedTokenDoesNotExist() public {
+        vm.expectRevert("Token does not exist");
+        tokens.removeAllowedToken(address(1));
+    }
+
+    function testRemoveAllowedToken() public {
+        vm.expectEmit();
+        uint8 decimals = 6;
+        string memory symbol = "USDT";
+        string memory name = "Tether USD";
+        emit Tokens.TokenAdded(address(1), decimals, CHAIN_ID, symbol, name);
+        tokens.addAllowedToken(address(1), decimals, symbol, name);
+        emit Tokens.TokenRemoved(address(1));
+        tokens.removeAllowedToken(address(1));
+    }
+
+    function testRemoveAllowedTokens() public {
+        vm.expectEmit();
+        emit Tokens.TokenAdded(address(1), 6, CHAIN_ID, "USDT", "Tether USD");
+        tokens.addAllowedToken(address(1), 6, "USDT", "Tether USD");
+
+        emit Tokens.TokenAdded(address(2), 6, CHAIN_ID, "USDC", "USD Coin");
+        tokens.addAllowedToken(address(2), 6, "USDC", "USD Coin");
+
+        emit Tokens.TokenAdded(address(3), 6, CHAIN_ID, "DAI", "Dai Stablecoin");
+        tokens.addAllowedToken(address(3), 6, "DAI", "Dai Stablecoin");
+
+        emit Tokens.TokenRemoved(address(2));
+        tokens.removeAllowedToken(address(2));
+
+        Tokens.AllowedToken memory token1 = tokens.getAllowedToken(address(1));
+        assertEq(token1.decimals, 6);
+        assertEq(token1.symbol, "USDT");
+        assertEq(token1.name, "Tether USD");
+
+        Tokens.AllowedToken memory token2 = tokens.getAllowedToken(address(2));
+        assertEq(token2.decimals, 0);
+        assertEq(token2.symbol, "");
+        assertEq(token2.name, "");
+
+        Tokens.AllowedToken memory token3 = tokens.getAllowedToken(address(3));
+        assertEq(token3.decimals, 6);
+        assertEq(token3.symbol, "DAI");
+        assertEq(token3.name, "Dai Stablecoin");
+    }
+
+    function testGetAllowedTokenZeroAddress() public view {
+        Tokens.AllowedToken memory token = tokens.getAllowedToken(address(0));
+        assertEq(token.decimals, 0);
+        assertEq(token.symbol, "");
+        assertEq(token.name, "");
+    }
+
+    function testGetAllowedTokenExist() public {
+        uint8 decimals = 6;
+        string memory symbol = "USDT";
+        string memory name = "Tether USD";
+        tokens.addAllowedToken(address(1), decimals, symbol, name);
+        Tokens.AllowedToken memory token = tokens.getAllowedToken(address(1));
+        assertEq(token.decimals, decimals);
+        assertEq(token.symbol, symbol);
+        assertEq(token.name, name);
+    }
+}
